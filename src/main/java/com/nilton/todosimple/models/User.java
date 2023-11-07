c846d0105f86a75b8f56bfce10d6c5387dd783e0
NiltonSilva <nsfgeo@hotmail.com>
2023-11-07 15:24:37 -0300

    realizado correções na entidade user

--- src/main/java/com/nilton/todosimple/models/User.java
+++ src/main/java/com/nilton/todosimple/models/User.java
@@ -1,6 +1,9 @@
 package com.nilton.todosimple.models;
 
-import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,41 +14,45 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.Objects;
 
 @Entity
 @Table(name = User.TABLE_NAME)
 public class User {
 
-    public interface CreateUser { }
-    public interface UpdateUser { }
+    public interface CreateUser {
+    }
+
+    public interface UpdateUser {
+    }
 
     public static final String TABLE_NAME = "user";
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // diz que é auto incremento
-    @Column(name = "id", unique = true) // da o nome pra coluna e diz que cada coluna é unica, não pode repetir nome
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "usaerName", length = 100, nullable = false, unique = true)
-    @NotNull (groups = {CreateUser.class, UpdateUser.class})
-    @NotEmpty (groups = {CreateUser.class, UpdateUser.class})
-    @Size(groups = CreateUser.class, min = 2, max = 100) 
-    private String userName;
-    
+    @Column(name = "userName", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
+    private String username;
+
+    @JsonProperty(access = Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
-    @NotNull (groups = CreateUser.class)
-    @NotEmpty (groups = CreateUser.class)
-    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
     // private List<Task> tasks = new ArrayList<Task>();
 
-    public User() { }
+    public User() {
+    }
 
-    public User(Long id, String userName, String password) {
+    public User(Long id, String username, String password) {
         this.id = id;
-        this.userName = userName;
+        this.username = username;
         this.password = password;
     }
 
@@ -58,11 +65,11 @@ public class User {
     }
 
     public String getUserName() {
-        return this.userName;
+        return this.username;
     }
 
     public void setUserName(String userName) {
-        this.userName = userName;
+        this.username = userName;
     }
 
     public String getPassword() {
@@ -75,27 +82,28 @@ public class User {
 
     @Override
     public boolean equals(Object obj) {
-        if(obj == this)
+        if (obj == this)
             return true;
-        if(obj == null)
+        if (obj == null)
             return false;
-        if(!(obj instanceof User))
+        if (!(obj instanceof User)) {
             return false;
+        }
         User other = (User) obj;
-        if(this.id == null)
-            if(other.id != null)
+        if (this.id == null)
+            if (other.id != null)
                 return false;
-            else if(!this.id.equals(other.id)) 
+            else if (!this.id.equals(other.id))
                 return false;
-        return Objects.equals(this.id, other.id) && Objects.equals(this.userName, other.userName) && Objects.equals(this.password, other.password);
-
+        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username)
+                && Objects.equals(this.password, other.password);
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime + result + ((this.id == null) ? 0 : this.id.hashCode());
+        result = prime * result + (this.id == null ? 0 : this.id.hashCode());
         return result;
     }
 
